1051f909a6529006ef95493d17dbdc16b2183bac
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-03-09 13:52:17 -0300

    Iniciar com class

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import header from './components/Header'
+import Header from './components/Header'
+
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <header/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return <h1>Hello from a class</h1>
+  }
+}
+
+export default App;
---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,9 @@
+const header = () => {
+    return (
+        <header>
+            <h1>Task TRacker</h1>
+        </header>
+    )
+}
+
+export default header
